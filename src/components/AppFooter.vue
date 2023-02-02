47f040555911abb3bc0acc451f6d7ff4bb185841
SamuelArico99 <samuel16699.sa@gmail.com>
2023-02-02 19:41:40 +0100

    impostazione footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,10 +8,77 @@ export default {
 
 <template>
     <footer>
-        FOOTER
+        <section class="merchandise">
+            <div class="container">
+                <div>
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <span>DIGITAL COMICS</span>
+                </div>
+                <div>
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <span>DIGITAL COMICS</span>
+                </div>
+                <div>
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <span>DIGITAL COMICS</span>
+                </div>
+                <div>
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <span>DIGITAL COMICS</span>
+                </div>
+                <div>
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <span>DIGITAL COMICS</span>
+                </div>
+            </div>
+
+        </section>
+        <section class="contacts-terms">
+
+        </section>
+        <section class="sign-up">
+
+        </section>
     </footer>
 </template>
 
 <style lang="scss" scoped>
+.merchandise {
+    width: 100%;
+    height: 250px;
+    background-color: #0282f9;
+    display: flex;
+    justify-content: center;
 
+    .container {
+        width: 70%;
+        height: 100%;
+        display: flex;
+
+        div {
+            height: 100%;
+            width: calc(100%/5);
+            display: flex;
+            align-items: center;
+
+            &:hover {
+                cursor: pointer;
+            }
+
+            img {
+                width: 80px;
+            }
+
+            span {
+                margin: 0 10px;
+                color: white;
+            }
+        }
+
+    }
+
+
+
+
+}
 </style>
