2466c6cb62c77679d846504b58c3f33a912fceeb
SamuelArico99 <samuel16699.sa@gmail.com>
2023-02-02 18:20:09 +0100

    impostazione header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,10 +8,90 @@ export default {
 
 <template>
     <header>
-        HEADER
+        <img src="../assets/img/dc-logo.png" alt="">
+        <ul>
+            <li>
+                <a href="#">CHARTERS</a>
+            </li>
+            <li>
+                <a href="#">COMICS</a>
+            </li>
+            <li>
+                <a href="#">MOVIES</a>
+            </li>
+            <li>
+                <a href="#">TV</a>
+            </li>
+            <li>
+                <a href="#">GAMES</a>
+            </li>
+            <li>
+                <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="#">VIDEOS</a>
+            </li>
+            <li>
+                <a href="#">FANS</a>
+            </li>
+            <li>
+                <a href="#">NEWS</a>
+            </li>
+            <li>
+                <a href="#">SHOP</a>
+            </li>
+        </ul>
     </header>
 </template>
 
 <style lang="scss" scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+    height: 150px;
 
+}
+
+img:hover {
+    cursor: pointer;
+}
+
+ul {
+    display: flex;
+    text-decoration: none;
+    height: 100%;
+    align-items: center;
+
+    li {
+        padding: 10px;
+        list-style: none;
+        height: 100%;
+
+        &:hover {
+            border-bottom: 2px solid #0282f9;
+
+        }
+
+
+        a {
+            color: black;
+            text-decoration: none;
+            height: 100%;
+            display: flex;
+            align-items: center;
+
+            &:hover {
+                color: #0282f9;
+            }
+
+
+        }
+
+
+
+    }
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,10 +8,27 @@ export default {
 
 <template>
     <main>
-        MAIN
+        <div>
+            --> Content goes here >--
+        </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
+main {
+    width: 100%;
+    height: 100px;
+    background-color: #1c1c1c;
 
+
+    div {
+        height: 100%;
+        width: 80%;
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+        font-size: 30px;
+        color: white;
+    }
+}
 </style>
